f3c2c05473ce9f65fae4e3f22ce511a042221153
RogerJung <engineer54rj@gmail.com>
2023-01-12 17:29:46 +0800

    Update: recording.sh

--- scripts/recording.sh
+++ scripts/recording.sh
@@ -5,10 +5,12 @@ cd "$script_dir"
 i=0
 while read line
 do 
-	array[$i]="$line"
+	array[$i]="/dev/v4l/by-id/$line"
 	let i=i+1
 done < ../config/camera-id.txt
 
+echo ${array[0]}
+exit
 parallel --lb --timeout 10 <<EOF
 tshark -i enp7s0 -w lidar.pcap udp
 ffmpeg -y -f video4linux2 -input_format uyvy422 -framerate 30 -video_size 1920x1080 -i ${array[0]} -vf transpose=2,transpose=2 video1.avi
