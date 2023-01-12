f2d74c77a92472f19be5d98d21e8c730976bdfd9
RogerJung <engineer54rj@gmail.com>
2023-01-12 17:18:05 +0800

    Update: recording.sh

--- scripts/recording.sh
+++ scripts/recording.sh
@@ -1,10 +1,17 @@
-video1=$(readlink -f /dev/v4l/by-id/usb-046d_Logitech_Webcam_C930e_B2FDD85E-video-index0 )
-video2=$(readlink -f /dev/v4l/by-id/usb-046d_Logitech_Webcam_C930e_13C47C2E-video-index0)
-video3=$(readlink -f /dev/v4l/by-id/usb-046d_Logitech_Webcam_C930e_F7637C2E-video-index0)
+
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$script_dir"
+
+i=0
+while read line
+do 
+	array[$i]="$line"
+	let i=i+1
+done < ../config/camera-id.txt
 
 parallel --lb --timeout 10 <<EOF
 tshark -i enp7s0 -w lidar.pcap udp
-ffmpeg -y -f video4linux2 -input_format uyvy422 -framerate 30 -video_size 1920x1080 -i $video1 -vf transpose=2,transpose=2 video1.avi
-ffmpeg -y -f video4linux2 -input_format uyvy422 -framerate 30 -video_size 1920x1080 -i $video2 -vf transpose=2,transpose=2 video2.avi
-ffmpeg -y -f video4linux2 -input_format uyvy422 -framerate 30 -video_size 1920x1080 -i $video3 -vf transpose=2,transpose=2 video3.avi
+ffmpeg -y -f video4linux2 -input_format uyvy422 -framerate 30 -video_size 1920x1080 -i ${array[0]} -vf transpose=2,transpose=2 video1.avi
+ffmpeg -y -f video4linux2 -input_format uyvy422 -framerate 30 -video_size 1920x1080 -i ${array[1]} -vf transpose=2,transpose=2 video2.avi
+ffmpeg -y -f video4linux2 -input_format uyvy422 -framerate 30 -video_size 1920x1080 -i ${array[2]} -vf transpose=2,transpose=2 video3.avi
 EOF
