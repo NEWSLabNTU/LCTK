6584c7c631812d3a76c4e0df06fad2b1e94b6207
newslab <philly12399@gmail.com>
2023-02-17 13:50:51 +0800

    Update: rsync script

--- scripts/record-data/rsync.sh
+++ scripts/record-data/rsync.sh
@@ -2,9 +2,10 @@
 set -e
 
 function print_usage() {
-    echo "example usage: $0 14 18 22" 
+    echo "example usage: $0 14 18 22 exp1" 
     echo "    sort ips by wayside 123" 
     echo "    to download data from 10.8.0.14 10.8.0.18 10.8.0.22"
+    echo "    data path: LCTK/scripts/record-data/waysidei/recname (optional, default is 'recording')"
 }
 
 #give wayside 123's ip
@@ -24,11 +25,13 @@ shift || {
     print_usage
     exit 1
 }
-mkdir -p ~/2023-camera-calibration/recording
-rsync -azvP newslab@"${ip1}":/home/newslab/LCTK/scripts/record-data/wayside1/recording/ \
- ~/2023-camera-calibration/recording
- rsync -azvP newslab@"${ip2}":/home/newslab/LCTK/scripts/record-data/wayside2/recording/ \
- ~/2023-camera-calibration/recording
- rsync -azvP newslab@"${ip3}":/home/newslab/LCTK/scripts/record-data/wayside3/recording/ \
- ~/2023-camera-calibration/recording
+recname="$1"
+recname="${recname:-"recording"}"
+mkdir -p ~/2023-camera-calibration/${recname}
+rsync -azvP newslab@"${ip1}":/home/newslab/LCTK/scripts/record-data/wayside1/${recname}/ \
+ ~/2023-camera-calibration/${recname}
+ rsync -azvP newslab@"${ip2}":/home/newslab/LCTK/scripts/record-data/wayside2/${recname}/ \
+ ~/2023-camera-calibration/${recname}
+ rsync -azvP newslab@"${ip3}":/home/newslab/LCTK/scripts/record-data/wayside3/${recname}/ \
+ ~/2023-camera-calibration/${recname}
 
