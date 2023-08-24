e2345fcfb3b6ba3dc869353c0f6ae4670c995136
jerry73204 <jerry73204@gmail.com>
2023-08-25 07:11:01 +0800

    [hollow-board-detector] Correct the board pose to ensure it stands right.

--- rust-lib/hollow-board-detector/src/detector.rs
+++ rust-lib/hollow-board-detector/src/detector.rs
@@ -7,8 +7,10 @@ use crate::{
 use anyhow::Result;
 use aruco_config::multi_aruco::MultiArucoPattern;
 use hollow_board_config::{BoardModel, BoardShape};
+use na::coordinates::XYZ;
 use nalgebra as na;
-use std::f64;
+use noisy_float::prelude::*;
+use std::f64::{self, consts::FRAC_PI_2};
 
 #[derive(Debug, Clone)]
 pub struct Detector {
@@ -68,15 +70,53 @@ impl Detector {
             }
         };
 
+        let board_model = BoardModel {
+            pose: board_pose,
+            board_shape: BoardShape {
+                board_width,
+                hole_radius,
+                hole_center_shift,
+            },
+            marker_paper_size,
+        };
+
+        // Correct the board pose to ensure it stands right.
+        let pose = {
+            let board_normal = board_model.board_z_axis();
+
+            let corners = [
+                board_model.bottom_corner(),
+                board_model.left_corner(),
+                board_model.top_corner(),
+                board_model.right_corner(),
+            ];
+            let (lowest_index, _) = corners
+                .iter()
+                .enumerate()
+                .min_by_key(|(_, point)| r64(point.z))
+                .unwrap();
+
+            // Compute the rotation using the by 90deg * index.
+            let fixup_rotation = {
+                let angle = FRAC_PI_2 * lowest_index as f64;
+                na::UnitQuaternion::from_axis_angle(&board_normal, angle)
+            };
+
+            // Use the position of the lowest point as the new translation.
+            let fixup_translation = {
+                let lowest_point = &corners[lowest_index];
+                let XYZ { x, y, z } = **lowest_point;
+                na::Translation3::new(x, y, z)
+            };
+
+            // Construct the corrected pose
+            fixup_translation * fixup_rotation * &board_model.pose.rotation
+        };
+
         let detection = Detection {
             board_model: BoardModel {
-                pose: board_pose,
-                board_shape: BoardShape {
-                    board_width,
-                    hole_radius,
-                    hole_center_shift,
-                },
-                marker_paper_size,
+                pose,
+                ..board_model
             },
             plane_ransac_data,
             icp_data,
