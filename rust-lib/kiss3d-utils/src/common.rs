f33ef078301b11d45f8141778881f06cccace45e
aeon <jerry73204@gmail.com>
2023-10-08 23:20:36 +0800

    Use kiss3d-utils on GitHub

--- rust-lib/kiss3d-utils/src/common.rs
+++
@@ -1,7 +0,0 @@
-pub use itertools::{iproduct, izip, Itertools as _};
-pub use kiss3d::window::Window;
-pub use nalgebra::{
-    self as na, Isometry3, Point3, Scalar, Translation3, Unit, UnitQuaternion, Vector3,
-};
-pub use simba::scalar::SupersetOf;
-pub use std::{borrow::Cow, f64};
--- rust-lib/kiss3d-utils/src/lib.rs
+++
@@ -1,6 +0,0 @@
-mod common;
-pub mod plot;
-pub mod pose_adjust;
-
-pub use plot::*;
-pub use pose_adjust::*;
