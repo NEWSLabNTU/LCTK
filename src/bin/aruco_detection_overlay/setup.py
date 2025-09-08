203ec7df152a934e8544df6263cd0720d65e7a5c
aeon <jerry73204@gmail.com>
2025-09-08 13:11:24 +0800

    Format aruco_detection_overlay code.

--- src/bin/aruco_detection_overlay/setup.py
+++ src/bin/aruco_detection_overlay/setup.py
@@ -1,25 +1,24 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-package_name = 'aruco_detection_overlay'
+package_name = "aruco_detection_overlay"
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version="0.1.0",
     packages=find_packages(),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='LCTK Team',
-    maintainer_email='calibration@example.com',
-    description='ArUco detection visualization overlay for RViz',
-    license='Apache-2.0',
+    maintainer="LCTK Team",
+    maintainer_email="calibration@example.com",
+    description="ArUco detection visualization overlay for RViz",
+    license="Apache-2.0",
     entry_points={
-        'console_scripts': [
-            'aruco_detection_overlay = aruco_detection_overlay.aruco_detection_overlay:main',
+        "console_scripts": [
+            "aruco_detection_overlay = aruco_detection_overlay.aruco_detection_overlay:main",
         ],
     },
 )
