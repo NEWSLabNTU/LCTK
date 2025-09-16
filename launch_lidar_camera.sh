5ebdc5a1eec5e7feff1421e30330c42811612f26
xXRpnx18 <asd1qwe456123@gmail.com>
2025-09-17 02:00:00 +0800

    adding RUST_LOG=debug to enable debug!() information

--- launch_lidar_camera.sh
+++ launch_lidar_camera.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source install/setup.bash
-ros2 launch calib_launch lidar_camera_calibration.launch.xml \
+RUST_LOG=debug ros2 launch calib_launch lidar_camera_calibration.launch.xml \
 		pcap_file:=$PWD/data/sampledata/3/lidar.pcap \
 		video_file:=$PWD/data/sampledata/3/video.avi \
 		loop:=true \
