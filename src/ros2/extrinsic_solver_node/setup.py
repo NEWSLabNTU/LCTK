2689b7194bab32d34ce88ab228e4f5b9a66cc522
aeon <jerry73204@gmail.com>
2025-09-19 10:39:41 +0800

    [extrinsic_solver_node] Fix naming in setup.py

--- src/ros2/extrinsic_solver_node/setup.py
+++ src/ros2/extrinsic_solver_node/setup.py
@@ -25,7 +25,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'extrinsic_solver_node = extrinsic_solver_node_py.main:main',
+            'extrinsic_solver_node = extrinsic_solver_node.main:main',
         ],
     },
 )
