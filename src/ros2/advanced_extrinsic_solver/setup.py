39f19e3b3a65514bf8b766b660e2edf3dff895f3
aeon <jerry73204@gmail.com>
2025-10-01 17:38:39 +0800

    [advanced_extrinsic_solver] Create an advanced extrinsic solver package.

---
+++ src/ros2/advanced_extrinsic_solver/setup.py
@@ -0,0 +1,31 @@
+import os
+from glob import glob
+
+from setuptools import find_packages, setup
+
+package_name = "advanced_extrinsic_solver"
+
+setup(
+    name=package_name,
+    version="0.1.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
+        # (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),  # No local config files
+        (os.path.join("lib", package_name), ["scripts/advanced_extrinsic_solver"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="NEWSLAB NTU",
+    maintainer_email="lctk@ntu.edu.tw",
+    description="Simple Python ROS 2 node for demonstrating solvePnP with ArUco and board detections",
+    license="MIT",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "advanced_extrinsic_solver = advanced_extrinsic_solver.main:main",
+        ],
+    },
+)
